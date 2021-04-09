dce3f47a542e625c1351a2c07ff989354b9973a5
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-04-08 21:00:01 -0400

    job stuff

--- src/models/github/job.ts
+++ src/models/github/job.ts
@@ -1,10 +1,90 @@
 interface Job {
   /**
    * The name of the job displayed on GitHub.
+   * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobsjob_idname
    */
   name?: string
 
+  /**
+   * Identifies any jobs that must complete successfully before this job will run. It can be a string or array of strings. If a job fails, all jobs that need it are skipped unless the jobs use a conditional expression that causes the job to continue.
+   * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobsjob_idneeds
+   */
   needs?: string | string[] // TODO: can we infer sibling job ids
+
+  /**
+   * The type of machine to run the job on. The machine can be either a GitHub-hosted runner or a self-hosted runner.
+   *
+   * GitHub-hosted runners
+   *
+   * If you use a GitHub-hosted runner, each job runs in a fresh instance of a virtual environment specified by runs-on.
+   *
+   * Available GitHub-hosted runner types are:
+   *
+   * Virtual environment	| YAML workflow label
+   * Windows Server 2019	| windows-latest or windows-2019
+   * Ubuntu 20.04	        | ubuntu-latest or ubuntu-20.04
+   * Ubuntu 18.04	        | ubuntu-18.04
+   * Ubuntu 16.04	        | ubuntu-16.04
+   * macOS Big Sur 11.0	  | macos-11.0
+   * macOS Catalina 10.15	| macos-latest or macos-10.15
+   *
+   * Self-hosted runners
+   *
+   * To specify a self-hosted runner for your job, configure runs-on in your workflow file with self-hosted runner labels.
+   *
+   * All self-hosted runners have the self-hosted label, and you can select any self-hosted runner by providing only the self-hosted label. Alternatively, you can use self-hosted in an array with additional labels, such as labels for a specific operating system or system architecture, to select only the runner types you specify.
+   *
+   * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobsjob_idruns-on
+   */
+  runsOn:
+    | 'windows-latest'
+    | 'windows-2019'
+    | 'ubuntu-latest'
+    | 'ubuntu-20.04'
+    | 'ubuntu-18.04'
+    | 'macos-11.0'
+    | 'macos-latest'
+    | 'macos-10.15'
+    | 'self-hosted'
+    | ['self-hosted', string]
+
+  /**
+   * The environment that the job references. All environment protection rules must pass before a job referencing the environment is sent to a runner. For more information, see "Environments."
+   *
+   * You can provide the environment as only the environment name, or as an environment object with the name and url. The URL maps to environment_url in the deployments API. For more information about the deployments API, see "Deployments."
+   *
+   * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobsjob_idenvironment
+   */
+  environment?: string | { name: string; url: string }
+
+  /**
+   * A map of outputs for a job. Job outputs are available to all downstream jobs that depend on this job. For more information on defining job dependencies, see jobs.<job_id>.needs.
+   *
+   * Job outputs are strings, and job outputs containing expressions are evaluated on the runner at the end of each job. Outputs containing secrets are redacted on the runner and not sent to GitHub Actions.
+   *
+   * To use job outputs in a dependent job, you can use the needs context. For more information, see [Context and expression syntax for GitHub Actions](https://docs.github.com/en/actions/reference/context-and-expression-syntax-for-github-actions#needs-context).
+   *
+   * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobsjob_idoutputs
+   */
+  outputs?: Record<string, string>
+
+  /**
+   * A map of default settings that will apply to all steps in the job. You can also set default settings for the entire workflow. For more information, see defaults.
+   *
+   * When more than one default setting is defined with the same name, GitHub uses the most specific default setting. For example, a default setting defined in a job will override a default setting that has the same name defined in a workflow.
+   *
+   * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobsjob_iddefaults
+   */
+  defaults?: any // TODO
+
+  /**
+   * You can use the if conditional to prevent a job from running unless a condition is met. You can use any supported context and expression to create a conditional.
+   *
+   * When you use expressions in an if conditional, you may omit the expression syntax (${{ }}) because GitHub automatically evaluates the if conditional as an expression, unless the expression contains any operators. If the expression contains any operators, the expression must be contained within ${{ }} to explicitly mark it for evaluation. For more information, see [Context and expression syntax for GitHub Actions](https://docs.github.com/en/actions/reference/context-and-expression-syntax-for-github-actions).
+   *
+   * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobsjob_idif
+   */
+  if?: string // TODO
 }
 
 export default Job
--- src/models/github/workflow.ts
+++ src/models/github/workflow.ts
@@ -1,4 +1,5 @@
 import * as events from './events'
+import type Job from './job'
 type Event = ReturnType<typeof events[keyof typeof events]>
 
 interface Workflow {
@@ -10,6 +11,7 @@ interface Workflow {
 
   /**
    * The name of the GitHub event that triggers the workflow. You can provide a single event string, array of events, array of event types, or an event configuration map that schedules a workflow or restricts the execution of a workflow to specific files, tags, or branch changes. For a list of available events, see "Events that trigger workflows."
+   *
    * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#on
    *
    * @example
@@ -33,6 +35,7 @@ interface Workflow {
    * A map of environment variables that are available to the steps of all jobs in the workflow. You can also set environment variables that are only available to the steps of a single job or to a single step. For more information, see jobs.<job_id>.env and jobs.<job_id>.steps[*].env.
    *
    * When more than one environment variable is defined with the same name, GitHub uses the most specific environment variable. For example, an environment variable defined in a step will override job and workflow variables with the same name, while the step executes. A variable defined for a job will override a workflow variable with the same name, while the job executes.
+   *
    * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#env
    */
   env?: Record<string, string>
@@ -41,9 +44,28 @@ interface Workflow {
    * A map of default settings that will apply to all jobs in the workflow. You can also set default settings that are only available to a job. For more information, see jobs.<job_id>.defaults.
    *
    * When more than one default setting is defined with the same name, GitHub uses the most specific default setting. For example, a default setting defined in a job will override a default setting that has the same name defined in a workflow.
+   *
    * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#defaults
    */
   defaults?: any // TODO
+
+  /**
+   * A workflow run is made up of one or more jobs. Jobs run in parallel by default. To run jobs sequentially, you can define dependencies on other jobs using the jobs.<job_id>.needs keyword.
+   *
+   * Each job runs in a runner environment specified by runs-on.
+   *
+   * You can run an unlimited number of jobs as long as you are within the workflow usage limits. For more information, see "Usage limits and billing" for GitHub-hosted runners and "About self-hosted runners" for self-hosted runner usage limits.
+   *
+   * If you need to find the unique identifier of a job running in a workflow run, you can use the GitHub API. For more information, see "Workflow Jobs."
+   *
+   * @link https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobs
+   * @see {@link Job}
+   */
+  jobs: Record<string, Job>
+}
+
+const w: Workflow = {
+  jobs: {},
 }
 
 export default Workflow
