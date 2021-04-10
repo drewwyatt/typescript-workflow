7c45c3d71fa6307eb6746f1047775db73f3640aa
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-04-10 16:00:24 -0400

    reorg + setup say-hello snapshot

--- src/__tests__/snaps.ts
+++ src/__tests__/snaps.ts
@@ -1,18 +1,4 @@
-import { pullRequest, push, release } from '../models'
-import { toYaml } from '../yaml'
+import toYaml from '../main'
+import sayHello from './workflows/say-hello'
 
-test('I can take a snapshot of an arbitrary string', () => {
-  expect(
-    toYaml({
-      name: 'My Sample Workflow',
-      on: [
-        pullRequest({ 'branches-ignore': ['foo', 'bar'], tags: ['wow'] }),
-        push({ branches: ['main'] }),
-        release(),
-      ],
-      jobs: {
-        'say-hello': { runsOn: 'ubuntu-latest', steps: [{ run: 'echo "hello, world"' }] },
-      },
-    }),
-  ).toMatchSnapshot()
-})
+test('say-hello', () => expect(toYaml(sayHello)).toMatchSnapshot())
