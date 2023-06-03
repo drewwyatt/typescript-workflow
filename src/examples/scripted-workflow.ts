ca3978024f5b4a5cb3304cff0201ba8542e9d4a5
Drew Wyatt <drew.j.wyatt@gmail.com>
2023-06-03 15:11:01 -0400

    jobThatSendsANotificationOnFailure

--- src/examples/scripted-workflow.ts
+++ src/examples/scripted-workflow.ts
@@ -1,17 +1,42 @@
+import type { NormalJob } from '~/schemas/github-workflow'
 import { workflow, run } from '~/.'
+
+// This would probably be imported from some shared location
+const jobThatSendsANotificationOnFailure = (
+  { slackChannel }: { slackChannel: string },
+  job: Omit<NormalJob, 'runs-on'>,
+): NormalJob => {
+  job.steps?.push({
+    if: '${{ failure() }}',
+    uses: './actions/slack-notifier',
+    with: {
+      channel: slackChannel,
+      token: '${{ secrets.SLACK_TOKEN }}',
+    },
+  })
+
+  // force some specific runner
+  ;(job as NormalJob)['runs-on'] = 'ubuntu-latest'
+
+  return job as NormalJob
+}
 
 export default workflow({
   name: 'Scripted Workflow',
   on: 'push',
   jobs: {
-    'scripted-job': {
-      'runs-on': 'ubuntu-latest',
-      steps: [
-        run('check it')`
+    'scripted-job': jobThatSendsANotificationOnFailure(
+      {
+        slackChannel: '#dev-alerts',
+      },
+      {
+        steps: [
+          run('check it')`
           echo "does this work?"
           echo "this has multiple lines"
         `,
-      ],
-    },
+        ],
+      },
+    ),
   },
 })
