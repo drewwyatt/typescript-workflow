7ae78042359db4e6cef7af25acbfc1edde6f7148
Drew Wyatt <drew.j.wyatt@gmail.com>
2023-06-02 22:07:46 -0400

    create a simple workflow

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello, world')
+import type { GithubWorkflow, NormalJob } from '~/schemas/github-workflow'
+
+export const workflow = (spec: GithubWorkflow): GithubWorkflow => spec
+export const job = (spec: NormalJob): NormalJob => spec
+
+export default workflow({
+  name: 'Simple Workflow',
+  on: 'push',
+  jobs: {
+    'say-hello': job({
+      'runs-on': 'ubuntu-latest',
+      steps: [{ run: 'echo "hello, world!"' }],
+    }),
+  },
+})
